87add19f1f7552d2c6927d9295eef1c8fd060e03
MuelZito <biduzin2607@gmail.com>
2024-05-16 17:00:54 -0300

    Criação das entidades Usuario e Jogo, criado o RetrofitInstance

---
+++ app/src/main/java/com/example/projetotestes/network/RetrofitInstance.kt
@@ -0,0 +1,14 @@
+package com.example.projetotestes.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val URL = "https://exemplo.com"
+
+    fun getIstance(): Retrofit{
+        return Retrofit.Builder().baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
